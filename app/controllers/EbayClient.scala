dc8f4be7ff1fdf747450ac0a8252da58893b0c0c
MarErm27 <v27i72@gmail.com>
2022-07-11 03:00:26 +0300

    using fold on getOrders

--- app/controllers/EbayClient.scala
+++ app/controllers/EbayClient.scala
@@ -2,22 +2,27 @@ package controllers
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.HttpResponse
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import akka.stream.Materializer
 import cats.data.EitherT
 import com.google.inject.Inject
 import sell.fulfillment.order.GetOrdersResponse
-
 import scala.concurrent.{ExecutionContext, Future}
 
 class EbayClient @Inject()()(implicit ec: ExecutionContext) {
   implicit val system = ActorSystem()
   implicit val mat: Materializer = Materializer(system)
   import system.dispatcher
+  val singleRequestHttpClient = { (req: HttpRequest) =>
+    Http().singleRequest(req)
+  }
+  val retryingHttpClient = { nextClient: (HttpRequest => Future[HttpResponse]) =>
+    req: HttpRequest => nextClient(req).flatMap(resp => if (resp.status.intValue >= 500) nextClient(req) else Future.successful(resp))
+  }
 
   // https://developer.ebay.com/api-docs/sell/fulfillment/resources/order/methods/getOrders
-  def getOrders(userToken: String): Future[GetOrdersResponse] = {
-    implicit val client = Http().singleRequest(_)
+  def getOrders(userToken: String): EitherT[Future, Either[Throwable, HttpResponse], GetOrdersResponse] = {
+    implicit val client = singleRequestHttpClient
     val orderClient = sell.fulfillment.order.OrderClient.httpClient(client)(ec, mat)
     orderClient.getOrders()
   }
--- test/controllers/EbayClientTest.scala
+++ test/controllers/EbayClientTest.scala
@@ -4,14 +4,16 @@ import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
-import play.api.test.Helpers.{await, defaultAwaitTimeout}
 import play.api.test.Helpers._
+import sell.fulfillment.order.GetOrdersResponse
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class EbayClientTest extends PlaySpec with GuiceOneServerPerSuite with ScalaFutures with IntegrationPatience {
+
   val ebayClient = app.injector.instanceOf(classOf[EbayClient])
   "EbayClientTest" must {
     "getOrders" in {
-      val res = await(ebayClient.getOrders(""))
+      val res: GetOrdersResponse = await(ebayClient.getOrders("").fold(l => l, r => r))
       res shouldBe "???"
     }
   }
